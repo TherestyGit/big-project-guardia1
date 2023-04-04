bcdd6265f529121d7b72fa1db8ca98798d224283
TherestyGit <alixwilmart23@outlook.com>
2023-04-04 20:19:12 +0200

    Dev de la page home

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+  $('.parallax').parallax();
+  $(".dropdown-trigger").dropdown({ hover: true, coverTrigger: false });
+});
+
+new WOW().init();
