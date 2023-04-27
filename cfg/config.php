317837a229305ac01e93395d54b4e4a6e4480e8e
TherestyGit <alixwilmart23@outlook.com>
2023-04-27 17:39:11 +0200

    finito

--- cfg/config.php
+++ cfg/config.php
@@ -4,8 +4,8 @@ session_start();
 
 $pdo = new PDO(
     'mysql:host=mysql-theresty.alwaysdata.net;dbname=theresty_bigproject;',
-    'theresty',
-    'rootbigproject',
+    'username',
+    'password',
     array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
 );
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
